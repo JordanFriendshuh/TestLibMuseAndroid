a067b51a65cbee4e24b91b2945ff3eb92f8285a5
Shannon Cox <shannon.cox00@gmail.com>
2015-04-19 09:28:54 -0500

    Initial commit

---
+++ app/src/main/java/com/interaxon/test/libmuse/MainActivity.java
@@ -0,0 +1,344 @@
+/**
+ * Example of using libmuse library on android.
+ * Interaxon, Inc. 2015
+ */
+
+package com.interaxon.test.libmuse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.interaxon.libmuse.ConnectionState;
+import com.interaxon.libmuse.Eeg;
+import com.interaxon.libmuse.LibMuseVersion;
+import com.interaxon.libmuse.Muse;
+import com.interaxon.libmuse.MuseArtifactPacket;
+import com.interaxon.libmuse.MuseConnectionListener;
+import com.interaxon.libmuse.MuseConnectionPacket;
+import com.interaxon.libmuse.MuseDataListener;
+import com.interaxon.libmuse.MuseDataPacket;
+import com.interaxon.libmuse.MuseDataPacketType;
+import com.interaxon.libmuse.MuseManager;
+import com.interaxon.libmuse.MusePreset;
+import com.interaxon.libmuse.MuseVersion;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * In this simple example MainActivity implements 2 MuseHeadband listeners
+ * and updates UI when data from Muse is received. Similarly you can implement
+ * listers for other data or register same listener to listen for different type
+ * of data.
+ * For simplicity we create Listeners as inner classes of MainActivity. We pass
+ * reference to MainActivity as we want listeners to update UI thread in this
+ * example app.
+ * You can also connect multiple muses to the same phone and register same
+ * listener to listen for data from different muses. In this case you will
+ * have to provide synchronization for data members you are using inside
+ * your listener.
+ *
+ * Usage instructions:
+ * 1. Enable bluetooth on your device
+ * 2. Pair your device with muse
+ * 3. Run this project
+ * 4. Press Refresh. It should display all paired Muses in Spinner
+ * 5. Make sure Muse headband is waiting for connection and press connect.
+ * It may take up to 10 sec in some cases.
+ * 6. You should see EEG and accelerometer data as well as connection status,
+ * Version information and MuseElements (alpha, beta, theta, delta, gamma waves)
+ * on the screen.
+ */
+public class MainActivity extends Activity implements OnClickListener {
+
+    /**
+     * Connection listener updates UI with new connection status and logs it.
+     */
+    class ConnectionListener extends MuseConnectionListener {
+
+        final WeakReference<Activity> activityRef;
+
+        ConnectionListener(final WeakReference<Activity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseConnectionPacket(MuseConnectionPacket p) {
+            final ConnectionState current = p.getCurrentConnectionState();
+            final String status = p.getPreviousConnectionState().toString() +
+                         " -> " + current;
+            final String full = "Muse " + p.getSource().getMacAddress() +
+                                " " + status;
+            Log.i("Muse Headband", full);
+            Activity activity = activityRef.get();
+            // UI thread is used here only because we need to update
+            // TextView values. You don't have to use another thread, unless
+            // you want to run disconnect() or connect() from connection packet
+            // handler. In this case creating another thread is required.
+            if (activity != null) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView statusText =
+                        (TextView) findViewById(R.id.con_status);
+                        statusText.setText(status);
+                        TextView museVersionText =
+                                (TextView) findViewById(R.id.version);
+                        if (current == ConnectionState.CONNECTED) {
+                            MuseVersion museVersion = muse.getMuseVersion();
+                            String version = museVersion.getFirmwareType() +
+                                 " - " + museVersion.getFirmwareVersion() +
+                                 " - " + Integer.toString(
+                                    museVersion.getProtocolVersion());
+                            museVersionText.setText(version);
+                        } else {
+                            museVersionText.setText(R.string.undefined);
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    /**
+     * Data listener will be registered to listen for: Accelerometer,
+     * Eeg and Relative Alpha bandpower packets. In all cases we will
+     * update UI with new values.
+     * We also will log message if Artifact packets contains "blink" flag.
+     * DataListener methods will be called from execution thread. If you are
+     * implementing "serious" processing algorithms inside those listeners,
+     * consider to create another thread.
+     */
+    class DataListener extends MuseDataListener {
+
+        final WeakReference<Activity> activityRef;
+
+        DataListener(final WeakReference<Activity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseDataPacket(MuseDataPacket p) {
+            switch (p.getPacketType()) {
+                case ALPHA_RELATIVE:
+                    updateAlphaRelative(p.getValues());
+                    break;
+                case BETA_RELATIVE:
+                    updateBetaRelative(p.getValues());
+                    break;
+                case GAMMA_RELATIVE:
+                    updateGammaRelative(p.getValues());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void receiveMuseArtifactPacket(MuseArtifactPacket p) {
+            if (p.getHeadbandOn() && p.getBlink()) {
+                Log.i("Artifacts", "blink");
+            }
+        }
+
+        //ALPHA RELATIVE == ELEMENTS, BITCH!!!
+        //com.interaxon.libmuse.MuseDataPacketType.ALPHA_RELATIVE
+        //com.interaxon.libmuse.MuseDataPacketType.BETA_RELATIVE
+        //com.interaxon.libmuse.MuseDataPacketType.GAMMA_RELATIVE
+
+        private void updateAlphaRelative(final ArrayList<Double> data) {
+            Activity activity = activityRef.get();
+            if (activity != null) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView alpha_value = (TextView) findViewById(R.id.alpha_value);
+                        double alpha_average = (data.get(Eeg.TP9.ordinal())
+                                + data.get(Eeg.FP1.ordinal())
+                                + data.get(Eeg.FP2.ordinal())
+                                + data.get(Eeg.TP10.ordinal()))
+                                / (double) 4 ;
+                        alpha_value.setText(String.format(
+                                "%6.2f", alpha_average));
+                    }
+                });
+            }
+        }
+
+
+        private void updateBetaRelative(final ArrayList<Double> data) {
+            Activity activity = activityRef.get();
+            if (activity != null) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView beta_value = (TextView) findViewById(R.id.beta_value);
+                        double beta_average = (data.get(Eeg.TP9.ordinal())
+                                + data.get(Eeg.FP1.ordinal())
+                                + data.get(Eeg.FP2.ordinal())
+                                + data.get(Eeg.TP10.ordinal()))
+                                / (double) 4 ;
+                        beta_value.setText(String.format(
+                                "%6.2f", beta_average));
+                    }
+                });
+            }
+        }
+
+        private void updateGammaRelative(final ArrayList<Double> data) {
+            Activity activity = activityRef.get();
+            if (activity != null) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView gamma_value = (TextView) findViewById(R.id.gamma_value);
+                        double gamma_average = (data.get(Eeg.TP9.ordinal())
+                                + data.get(Eeg.FP1.ordinal())
+                                + data.get(Eeg.FP2.ordinal())
+                                + data.get(Eeg.TP10.ordinal()))
+                                / (double) 4 ;
+                        gamma_value.setText(String.format(
+                                "%6.2f", gamma_average));
+                    }
+                });
+            }
+        }
+    }
+
+    private Muse muse = null;
+    private ConnectionListener connectionListener = null;
+    private DataListener dataListener = null;
+    private boolean dataTransmission = true;
+
+    public MainActivity() {
+        // Create listeners and pass reference to activity to them
+        WeakReference<Activity> weakActivity =
+                                new WeakReference<Activity>(this);
+        connectionListener = new ConnectionListener(weakActivity);
+        dataListener = new DataListener(weakActivity);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        Button refreshButton = (Button) findViewById(R.id.refresh);
+        refreshButton.setOnClickListener(this);
+        Button connectButton = (Button) findViewById(R.id.connect);
+        connectButton.setOnClickListener(this);
+        Button disconnectButton = (Button) findViewById(R.id.disconnect);
+        disconnectButton.setOnClickListener(this);
+        Button pauseButton = (Button) findViewById(R.id.pause);
+        pauseButton.setOnClickListener(this);
+        Log.i("Muse Headband", "libmuse version=" + LibMuseVersion.SDK_VERSION);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Spinner musesSpinner = (Spinner) findViewById(R.id.muses_spinner);
+        if (v.getId() == R.id.refresh) {
+            MuseManager.refreshPairedMuses();
+            List<Muse> pairedMuses = MuseManager.getPairedMuses();
+            List<String> spinnerItems = new ArrayList<String>();
+            for (Muse m: pairedMuses) {
+                String dev_id = m.getName() + "-" + m.getMacAddress();
+                Log.i("Muse Headband", dev_id);
+                spinnerItems.add(dev_id);
+            }
+            ArrayAdapter<String> adapterArray = new ArrayAdapter<String> (
+                    this, android.R.layout.simple_spinner_item, spinnerItems);
+            musesSpinner.setAdapter(adapterArray);
+        }
+        else if (v.getId() == R.id.connect) {
+            List<Muse> pairedMuses = MuseManager.getPairedMuses();
+            if (pairedMuses.size() < 1 ||
+                musesSpinner.getAdapter().getCount() < 1) {
+                Log.w("Muse Headband", "There is nothing to connect to");
+            }
+            else {
+                muse = pairedMuses.get(musesSpinner.getSelectedItemPosition());
+                ConnectionState state = muse.getConnectionState();
+                if (state == ConnectionState.CONNECTED ||
+                    state == ConnectionState.CONNECTING) {
+                    Log.w("Muse Headband", "doesn't make sense to connect second time to the same muse");
+                    return;
+                }
+                configure_library();
+                /**
+                 * In most cases libmuse native library takes care about
+                 * exceptions and recovery mechanism, but native code still
+                 * may throw in some unexpected situations (like bad bluetooth
+                 * connection). Print all exceptions here.
+                 */
+                try {
+                    muse.runAsynchronously();
+                } catch (Exception e) {
+                    Log.e("Muse Headband", e.toString());
+                }
+            }
+        }
+        else if (v.getId() == R.id.disconnect) {
+            if (muse != null) {
+                /**
+                 * true flag will force libmuse to unregister all listeners,
+                 * BUT AFTER disconnecting and sending disconnection event.
+                 * If you don't want to receive disconnection event (for ex.
+                 * you call disconnect when application is closed), then
+                 * unregister listeners first and then call disconnect:
+                 * muse.unregisterAllListeners();
+                 * muse.disconnect(false);
+                 */
+                muse.disconnect(true);
+            }
+        }
+        else if (v.getId() == R.id.pause) {
+            dataTransmission = !dataTransmission;
+            if (muse != null) {
+                muse.enableDataTransmission(dataTransmission);
+            }
+        }
+    }
+
+    private void configure_library() {
+        muse.registerConnectionListener(connectionListener);
+        muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.ALPHA_RELATIVE);
+        muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.BETA_RELATIVE);
+        muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.GAMMA_RELATIVE);
+        muse.setPreset(MusePreset.PRESET_14);
+        muse.enableDataTransmission(dataTransmission);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
